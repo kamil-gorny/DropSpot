b81ed8e802b8976c8b01cf56bf19f3b8a578793e
Kamil Górny <kamil.gorny99@gmail.com>
2024-09-16 22:05:53 +0200

    chore: Add Logo component

---
+++ src/lib/components/Logo.svelte
@@ -0,0 +1,3 @@
+<div class="bg-black text-white italic font-bold text-3xl p-2">
+    DROPSPOT
+</div>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+    import '../app.css';
+    import Logo from "$lib/components/Logo.svelte";
+</script>
+
+<div class="flex justify-center items-center pt-12">
+    <Logo/>
+</div>
+<slot></slot>
