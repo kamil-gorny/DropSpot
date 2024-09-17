f3234ba8aa97c59585c51a13db9f423fdc230a31
Kamil Górny <kamil.gorny99@gmail.com>
2024-09-17 18:15:15 +0200

    feat: Change a tags to NavigationItems in Navigation

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -1,15 +1,18 @@
+<script>
+    import NavigationItem from "./NavigationItem.svelte";
+
+</script>
 <div class="flex flex-col font-thin items-end w-1/6 text-sm">
-    <a href="">new</a>
-    <a href="">jackets</a>
-    <a href="">shirts</a>
-    <a href="">tops/sweaters</a>
-    <a href="">sweatshirts</a>
-    <a href="">pants</a>
-    <a href="">t-shirts</a>
-    <a href="">hats</a>
-    <a href="">bags</a>
-    <a href="">accessories</a>
-    <a href="">shoes</a>
-    <a href="">skate</a>
-    <a href="">all</a>
+    <NavigationItem link="" text="new"/>
+    <NavigationItem link="" text="jackets"/>
+    <NavigationItem link="" text="shirts"/>
+    <NavigationItem link="" text="tops/sweaters"/>
+    <NavigationItem link="" text="sweatshirts"/>
+    <NavigationItem link="" text="pants"/>
+    <NavigationItem link="" text="t-shirts"/>
+    <NavigationItem link="" text="bags"/>
+    <NavigationItem link="" text="accessories"/>
+    <NavigationItem link="" text="shoes"/>
+    <NavigationItem link="" text="skate"/>
+    <NavigationItem link="" text="all"/>
 </div>
---
+++ src/lib/components/NavigationItem.svelte
@@ -0,0 +1,6 @@
+<script>
+    export let link;
+    export let text;
+</script>
+
+<a class="hover:bg-black hover:text-white" href={link}>{text}</a>
