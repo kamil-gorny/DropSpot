73e33159b5f5fbbbdde120053691cd12606c33d7
Kamil Górny <kamil.gorny99@gmail.com>
2024-09-17 16:48:10 +0200

    feat: Add ProductTile component

---
+++ src/lib/components/ProductTile.svelte
@@ -0,0 +1,12 @@
+<script>
+    import tshirt from '$lib/images/tshirt.webp';
+    export let name;
+    export let price;
+    export let image;
+
+</script>
+<div class="w-[250px]">
+    <img src={image} alt="tshirt"/>
+    <p class="text-xs">{name}</p>
+    <p class="text-xs">${price}</p>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,14 @@
 <script>
     import Navigation from "$lib/components/Navigation.svelte";
+    import ProductTile from "../lib/components/ProductTile.svelte";
+    import tshirt from "$lib/images/tshirt.webp";
 </script>
 
+<div class="flex flex-row justify-center mt-28 gap-8">
 <Navigation/>
+
+<div class="grid grid-cols-3 gap-4">
+<ProductTile image={tshirt} name="Custom T-shirt" price="20"/>
+
+</div>
+</div>
