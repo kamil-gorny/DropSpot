eeb52973aac2277292e4afd70f6f86fe1438350d
Kamil Górny <kamil.gorny99@gmail.com>
2024-09-21 20:24:48 +0200

    feat: Fetch product from api

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,7 +1,10 @@
-import { products } from './data.js';
-
-export function load(){
-    return {
-        products
-    };
+export async function load(){
+    const url = "http://localhost:5170/api/product";
+    try{
+        const result = await fetch(url);
+        return await result.json();
+    }
+    catch (e){
+        console.log(e);
+    }
 }
