f34cb42b7cb27ed89d8d4bdea5c4b3d8285b7c91
Kamil Górny <kamil.gorny99@gmail.com>
2024-09-28 17:42:15 +0200

    feat: Adjust layout and place login button

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,18 @@
 <script>
     import '../app.css';
     import Logo from "$lib/components/Logo.svelte";
+    import Icon from "@iconify/svelte";
 </script>
 
-<div class="flex justify-center items-center pt-12">
-    <Logo/>
+<div class="flex items-center justify-between pt-12">
+    <div class="w-1/3"></div>
+    <div class="w-1/3 flex justify-center">
+        <Logo/>
+    </div>
+
+    <a href="/login" class="w-1/3 flex justify-end">
+        <p class="pr-2">login</p> <Icon class="mr-20 text-2xl hover:cursor-pointer" icon="ic:baseline-login"/>
+    </a>
 </div>
 
 <div class="container w-3/5 m-auto">
