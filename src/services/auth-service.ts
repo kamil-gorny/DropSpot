476f7cd2065284e30b384be4e12194f8359275fe
Kamil Górny <kamil.gorny99@gmail.com>
2024-10-01 20:35:02 +0200

    feat: Save token information in store

--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -3,21 +3,46 @@ import { PUBLIC_API_URL } from '$env/static/public';
 type User = {
     email: string;
     token: string;
-
 }
-export const userStore = writable<User>();
-export const login = async (email, password) => {
+
+type LoginRequest = {
+    email: string;
+    password: string;
+}
+type TokenResponse = {
+    tokenType: string;
+    accessToken: string;
+    refreshToken: string;
+    expiresIn: number;
+}
+export const tokenStore = writable<TokenResponse | null>(null);
+
+export const login  = async (loginRequest : LoginRequest)=> {
     const url = `${PUBLIC_API_URL}/auth/login`;
     const response = await fetch(url, {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
-        body: JSON.stringify({ email, password })
+        body: JSON.stringify(loginRequest)
+    });
+    if (response.ok) {
+        const data: TokenResponse = await response.json();
+        tokenStore.set(data);
+    }
+};
+
+export const register = async (firstName, lastName, email, password) => {
+    const url = `${PUBLIC_API_URL}/auth/register`;
+    const response = await fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ firstName, lastName, email, password })
     });
     if (response.ok) {
         const data = await response.json();
-        userStore.set(data);
     } else {
 
     }
