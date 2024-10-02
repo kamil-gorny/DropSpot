46d5ba2d7ec796966a79bab4270c731c7497c66a
Kamil Górny <kamil.gorny99@gmail.com>
2024-10-02 20:44:32 +0200

    feat: Adjust layout to user session

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -2,6 +2,8 @@
     import '../../app.css';
     import Logo from "$lib/components/Logo.svelte";
     import Icon from "@iconify/svelte";
+    import { get } from 'svelte/store';
+    import {tokenStore} from "../../services/auth-service.ts";
 </script>
 
 <div class="flex items-center justify-between pt-12">
@@ -11,7 +13,13 @@
     </div>
 
     <a href="/login" class="w-1/3 flex justify-end">
-        <p class="pr-2">login</p> <Icon class="mr-20 text-2xl hover:cursor-pointer" icon="ic:baseline-login"/>
+        {#if get(tokenStore) == null}
+            <p class="pr-2">login</p> <Icon class="mr-20 text-2xl hover:cursor-pointer" icon="ic:baseline-login"/>
+            {:else}
+            <button on:click={() => {tokenStore.set(null);}} class="pr-2">logout</button> <Icon class="mr-20 text-2xl hover:cursor-pointer" icon="ic:baseline-logout"/>
+
+        {/if}
+
     </a>
 </div>
 
