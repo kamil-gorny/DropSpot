5660edc3e96f4ad802eca0218c8a05ad79827a2c
Kamil Górny <kamil.gorny99@gmail.com>
2024-10-03 21:01:32 +0200

    feat: Call register function from signup

--- src/routes/(auth)/signup/+page.svelte
+++ src/routes/(auth)/signup/+page.svelte
@@ -1,17 +1,30 @@
+<script>
+    import {register} from "../../../services/auth-service.ts";
+    let firstName = "";
+    let lastName = "";
+    let email = "";
+    let password = "";
+</script>
+
 <div class="max-w-md mr-auto ml-auto h-screen flex flex-col">
     <span class="text-xl font-bold pt-14 pb-10">Sign up</span>
-    <form class="flex flex-col gap-6 ">
+    <form on:submit|preventDefault={register({
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        password: password
+    })} class="flex flex-col gap-6 ">
         <div class="flex flex-col">
             <label for="firstName">First name</label>
-            <input name="firstName" type="text" class="border rounded h-12"/>
+            <input name="firstName" type="text" class="border rounded h-12" bind:value={firstName}/>
         </div>
         <div class="flex flex-col">
             <label for="lastName">Last name</label>
-            <input name="lastName" type="text" class="border rounded h-12"/>
+            <input name="lastName" type="text" class="border rounded h-12" bind:value={lastName}/>
         </div>
         <div class="flex flex-col">
             <label for="email">Email</label>
-            <input name="email" type="text" class="border rounded h-12"/>
+            <input name="email" type="text" class="border rounded h-12" bind:value={email}/>
         </div>
         <div class="flex flex-col">
             <div class="flex justify-between">
@@ -19,7 +32,7 @@
 
             </div>
 
-            <input  name="password" type="password" class="border rounded h-12"/>
+            <input  name="password" type="password" class="border rounded h-12" bind:value={password}/>
         </div>
         <button class="bg-black text-white h-12">Create</button>
 
