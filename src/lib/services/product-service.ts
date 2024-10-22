d27f2a36817c2ef3ba626d4582a9487352788640
Kamil Górny <kamil.gorny99@gmail.com>
2024-10-22 20:40:28 +0200

    feat: Add GetProduct

--- src/lib/services/product-service.ts
+++ src/lib/services/product-service.ts
@@ -1,13 +1,25 @@
 import type {Product} from "../../models/Product";
 
     export async function GetProducts() : Promise<Product[]> {
-    const url = "http://localhost:5170/api/product";
-    try{
-        const result = await fetch(url);
-        return await result.json() as Product[];
+        const url = "http://localhost:5170/api/product";
+        try {
+            const result = await fetch(url);
+            return await result.json() as Product[];
+        } catch (e) {
+            console.log(e);
+            throw e;
+        }
     }
-    catch (e){
-        console.log(e);
-        throw e;
+
+    export async function GetProduct(id: string) : Promise<Product> {
+        const url = `http://localhost:5170/api/product/${id}`;
+
+        try{
+            const result = await fetch(url);
+            return await result.json() as Product;
+        }
+        catch (e){
+            console.log(e);
+            throw e;
+            }
     }
-}
